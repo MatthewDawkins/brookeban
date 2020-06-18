8c36af640f4eaa2cbe86b0f9d6cfea3e82bfe341
Skyla <skylayreiner@gmail.com>
2020-06-18 07:23:47 -0700

    progress...

---
+++ libs/placeholder-data.js
@@ -0,0 +1,54 @@
+// Placeholder data/img for services
+const imgURL = "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQHqid_OwecdbGP8HwYzu89xuD6Vkb8u4WoiYfibfhMMaIXgpbo&usqp=CAU";
+
+const servicePlaceHolder = [
+  {
+      id: "service-01",
+      title: "serviceTest1",
+      img: imgURL,
+      alt: "abc",
+      description: "1st description string",
+      info2: "2nd info string"
+  },
+  {
+      id: "service-02",
+      title: "serviceTest2",
+      img: imgURL,
+      alt: "abc",
+      description: "1st description string",
+      info2: "2nd info string"
+  },
+  {
+    id: "service-03",
+    title: "serviceTest3",
+    img: imgURL,
+    alt: "abc",
+    description: "1st description string",
+    info2: "2nd info string"
+  },
+  {
+    id: "service-04",
+    title: "serviceTest4",
+    img: imgURL,
+    alt: "abc",
+    description: "1st description string",
+    info2: "2nd info string"
+  },
+  {
+    id: "service-05",
+    title: "serviceTest5",
+    img: imgURL,
+    alt: "abc",
+    description: "1st description string",
+    info2: "2nd info string"
+  },
+  {
+    id: "service-06",
+    title: "serviceTest6",
+    img: imgURL,
+    alt: "abc",
+    description: "1st description string",
+    info2: "2nd info string"
+  }
+];
+module.exports = servicePlaceHolder;
