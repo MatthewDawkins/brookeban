3b5b2db2469d715a07c64d9ccffcc67b9e0a76d7
mageiden <60712721+mageiden@users.noreply.github.com>
2020-06-19 16:27:51 -0700

    q8)

--- libs/placeholder-data.js
+++ libs/placeholder-data.js
@@ -3,52 +3,84 @@ const imgURL = "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQHqid_Ow
 
 const servicePlaceHolder = [
   {
-      id: "service-01",
-      title: "serviceTest1",
+    id: "service-01",
+    title: "Painting",
+    img: imgURL,
+    description: "Small painting description",
+    info: "some small info about painting",
+    subservices:[{
+      subTitle: "Painting!!",
+      subImg: imgURL,
+      subDescription: "abc",
+      subAlt: "abc"
+  }]
+},
+  {
+    id: "service-02",
+    title: "Drywall",
+    img: imgURL,
+    description: "Small drywall description",
+    info: "some small info about drywall",
+    subservices: [{
+        subTitle: "abcdef",
+        subImg: imgURL,
+        subDescription: "abcd",
+        subAlt: "abc"
+    }]
+  },
+    {
+      id: "service-03",
+      title: "anotha1",
       img: imgURL,
-      alt: "abc",
-      description: "1st description string",
-      info2: "2nd info string"
-  },
-  {
-      id: "service-02",
-      title: "serviceTest2",
+      description: "anotha1",
+      info: "dunno",
+      subservices:[{
+        subTitle: "abcdef",
+        subImg: imgURL,
+        subDescription: "abcd",
+        subAlt: "abc"
+      }]
+    },
+    {
+      id: "service-04",
+      title: "anotha4",
       img: imgURL,
-      alt: "abc",
-      description: "1st description string",
-      info2: "2nd info string"
-  },
-  {
-    id: "service-03",
-    title: "serviceTest3",
-    img: imgURL,
-    alt: "abc",
-    description: "1st description string",
-    info2: "2nd info string"
-  },
-  {
-    id: "service-04",
-    title: "serviceTest4",
-    img: imgURL,
-    alt: "abc",
-    description: "1st description string",
-    info2: "2nd info string"
-  },
-  {
-    id: "service-05",
-    title: "serviceTest5",
-    img: imgURL,
-    alt: "abc",
-    description: "1st description string",
-    info2: "2nd info string"
-  },
-  {
-    id: "service-06",
-    title: "serviceTest6",
-    img: imgURL,
-    alt: "abc",
-    description: "1st description string",
-    info2: "2nd info string"
-  }
-];
+      description: "anotha4",
+      info: "dunno",
+      subservices:[{
+        subTitle: "abcdef",
+        subImg: imgURL,
+        subDescription: "abcd",
+        subAlt: "abc"
+      }]
+    },
+
+    {
+      id: "service-05",
+      title: "anotha5",
+      img: imgURL,
+      description: "anotha5",
+      info: "dunno",
+      subservices:[{
+        subTitle: "abcdef",
+        subImg: imgURL,
+        subDescription: "abcd",
+        subAlt: "abc"
+      }]
+    },
+    {
+      id: "service-06",
+      title: "anotha6",
+      img: imgURL,
+      description: "anotha6",
+      info: "dunno",
+      subservices:[{
+        subTitle: "abcdef",
+        subImg: imgURL,
+        subDescription: "abcd",
+        subAlt: "abc"
+      }]
+    }
+]
+
 module.exports = servicePlaceHolder;
