af00a6ffac4ad2a18a524a915ab5ff662deae973
Skyla <skylayreiner@gmail.com>
2020-06-23 19:02:04 -0700

    made progress on photogallery and carousel

--- libs/placeholder-gallery-data.js
+++ libs/placeholder-gallery-data.js
@@ -3,19 +3,60 @@ const photo = "PWLmnkk.jpg";
 const placeholderGallery = [
     {
         serviceInPhotoById: "service-01",
-        photo: photo,
-        alt: "photo-01-service-01"
+        photo: "placeholder_01.jpg",
+        title: "Painting" 
     },
     {
         serviceInPhotoById: "service-02",
         photo: photo,
-        alt: "photo-02-service-02"
+        title: "Drywall" 
     },
     {
         serviceInPhotoById: "service-03",
         photo: photo,
-        alt: "photo-03-service-03"
-    }
+        title: "anotha3" 
+    },
+    {
+      serviceInPhotoById: "service-01",
+      photo: "placeholder_01.jpg",
+      title: "Painting" 
+  },
+  {
+      serviceInPhotoById: "service-02",
+      photo: "placeholder_02.jpg",
+      alt: "photo-02-service-02"
+  },
+  {
+      serviceInPhotoById: "service-03",
+      photo: photo,
+      title: "anotha3" 
+  },
+  {
+    serviceInPhotoById: "service-01",
+    photo: photo,
+    title: "Painting" 
+},
+{
+    serviceInPhotoById: "service-02",
+    photo: photo,
+    title: "Drywall" 
+},
+{
+    serviceInPhotoById: "service-03",
+    photo: photo,
+    title: "anotha5" 
+},
+{
+  serviceInPhotoById: "service-05",
+  photo: photo,
+  title: "anotha5" 
+
+},
+{
+  serviceInPhotoById: "service-05",
+  photo: "placeholder_02.jpg",
+  title: "anotha5" 
+}
 ];
 
 module.exports = placeholderGallery;
