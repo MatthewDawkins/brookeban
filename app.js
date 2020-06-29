d206fac04e6e764f9f5633af2765c0595f0aac94
Matt <mattd1997@hotmail.com>
2020-06-29 02:53:34 -0400

    add google cloud sdk settings

--- app.js
+++ app.js
@@ -85,6 +85,7 @@ app.get("/services/:serviceId", function(req, res) {
 
 
 //Set up local host
-app.listen(3000, function() {
-  console.log("server is running on port 3000");
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, function() {
+  console.log("server is running on port ${PORT}");
 });
